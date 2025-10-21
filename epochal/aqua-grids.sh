80fb60d1dc950fb9529f6ad0c0f802b4345bec27
oloapinivad <p.davini@isac.cnr.it>
2025-10-21 14:39:40 +0000

    update aqua grid generator

--- epochal/aqua-grids.sh
+++ epochal/aqua-grids.sh
@@ -4,41 +4,68 @@ DIR=/lus/h2resw01/hpcperm/ccpd/AQUAGRID
 
 # cleanup
 module load cdo/2.4.0
+module load nco/5.3.2
 
 rm -rf $DIR
 mkdir -p $DIR
 
-#ORCA2
 cdo="cdo -f nc1 -b 32"
 
-#area
-$cdo -expr,areat=e1t*e2t /lus/h2resw01/hpcperm/ccpd/ECE4-DATA/nemo/domain/ORCA2/domain_cfg.nc $DIR/cellarea_ORCA2.nc
-
-# ORCA2
+#----ORCA2------#
 exp=TN10
 user=ccpd
 mkdir -p $DIR/ORCA2
 $cdo gtc,0 -setname,mask -selname,sos -seltimestep,1 /lus/h2resw01/scratch/${user}/ece4/${exp}/output/nemo/${exp}_oce_1m_T_1990-1990.nc $DIR/ORCA2/ORCA2_mesh_sfc_grid_T.nc
 $cdo gtc,0 -setname,mask -seltimestep,1 -selname,thetao /lus/h2resw01/scratch/${user}/ece4/${exp}/output/nemo/${exp}_oce_1m_T_1990-1990.nc $DIR/ORCA2/ORCA2_mesh_3d_grid_T.nc
-#rm -f $DIR/tmp.nc
-
-#PALEORCA
+for file in $DIR/ORCA2/ORCA2_mesh_sfc_grid_T.nc $DIR/ORCA2/ORCA2_mesh_3d_grid_T.nc ; do
+  ncrename -d x_grid_T,x $file
+  ncrename -d y_grid_T,y $file
+done
 
 #area
-$cdo -expr,areat=e1t*e2t /lus/h2resw01/hpcperm/ccpd/ECE4-DATA/nemo/domain/PALEORCA2/domain_cfg.nc $DIR/cellarea_PALEORCA2.nc
+$cdo -expr,area=e1t*e2t /lus/h2resw01/hpcperm/ccpd/ECE4-DATA/nemo/domain/ORCA2/domain_cfg.nc $DIR/ORCA2/cellarea_ORCA2_T.nc
+
+#----PALEORCA-----#
 
 # PALEORCA-PD
 exp=LC05
 user=ecme3497
-mkdir -p $DIR/PALEORCA2
-$cdo gtc,0 -setname,mask -seltimestep,1 -selname,sos /lus/h2resw01/scratch/${user}/ece4/${exp}/output/nemo/${exp}_oce_1m_T_1990-1990.nc $DIR/PALEORCA2/PALEORCA2_mesh_sfc_grid_T-pd.nc
-$cdo gtc,0 -setname,mask -seltimestep,1 -selname,so /lus/h2resw01/scratch/${user}/ece4/${exp}/output/nemo/${exp}_oce_1m_T_1990-1990.nc $DIR/PALEORCA2/PALEORCA2_mesh_3d_grid_T-pd.nc
-
-
+grid=PALEORCA2
+window=pd
+mkdir -p $DIR/$grid
+$cdo gtc,0 -setname,mask -seltimestep,1 -selname,sos /lus/h2resw01/scratch/${user}/ece4/${exp}/output/nemo/${exp}_oce_1m_T_1990-1990.nc $DIR/PALEORCA2/${grid}_mesh_sfc_grid_T-${window}.nc
+$cdo gtc,0 -setname,mask -seltimestep,1 -selname,so /lus/h2resw01/scratch/${user}/ece4/${exp}/output/nemo/${exp}_oce_1m_T_1990-1990.nc $DIR/PALEORCA2/${grid}_mesh_3d_grid_T-${window}.nc
 
 # PALEORCA-EO
 exp=EP01
 user=ecme3497
-mkdir -p $DIR/PALEORCA2
-$cdo gtc,0 -setname,mask -seltimestep,1 -selname,sos /lus/h2resw01/scratch/${user}/ece4/${exp}/output/nemo/${exp}_oce_1m_T_1990-1990.nc $DIR/PALEORCA2/PALEORCA2_mesh_sfc_grid_T-eo.nc
-$cdo gtc,0 -setname,mask -seltimestep,1 -selname,thetao /lus/h2resw01/scratch/${user}/ece4/${exp}/output/nemo/${exp}_oce_1m_T_1990-1990.nc $DIR/PALEORCA2/PALEORCA2_mesh_3d_grid_T-eo.nc
+window=eo
+$cdo gtc,0 -setname,mask -seltimestep,1 -selname,sos /lus/h2resw01/scratch/${user}/ece4/${exp}/output/nemo/${exp}_oce_1m_T_1990-1990.nc $DIR/PALEORCA2/${grid}_mesh_sfc_grid_T-${window}.nc
+$cdo gtc,0 -setname,mask -seltimestep,1 -selname,thetao /lus/h2resw01/scratch/${user}/ece4/${exp}/output/nemo/${exp}_oce_1m_T_1990-1990.nc $DIR/PALEORCA2/${grid}_mesh_3d_grid_T-${window}.nc
+
+for window in pd eo ; do
+  for file in sfc 3d ; do
+    ncrename -d x_grid_T,x $DIR/PALEORCA2/${grid}_mesh_${file}_grid_T-${window}.nc
+    ncrename -d y_grid_T,y $DIR/PALEORCA2/${grid}_mesh_${file}_grid_T-${window}.nc
+  done
+done
+
+#area
+$cdo -expr,area=e1t*e2t /lus/h2resw01/hpcperm/ccpd/ECE4-DATA/nemo/domain/PALEORCA2/domain_cfg.nc $DIR/PALEORCA2/cellarea_PALEORCA2_T.nc
+
+
+exit
+
+#-----eORCA1-----#
+exp=
+user=jjvh
+mkdir -p $DIR/ORCA2
+$cdo gtc,0 -setname,mask -selname,sos -seltimestep,1 /lus/h2resw01/scratch/${user}/ece4/${exp}/output/nemo/${exp}_oce_1m_T_1990-1990.nc $DIR/eORCA1/eORCA1_mesh_sfc_grid_T.nc
+$cdo gtc,0 -setname,mask -seltimestep,1 -selname,thetao /lus/h2resw01/scratch/${user}/ece4/${exp}/output/nemo/${exp}_oce_1m_T_1990-1990.nc $DIR/eORCA1/eORCA1_mesh_3d_grid_T.nc
+for file in $DIR/eORCA1/eORCA1_mesh_sfc_grid_T.nc $DIR/eORCA1/eORCA1_mesh_3d_grid_T.nc ; do
+  ncrename -d x_grid_T,x $file
+  ncrename -d y_grid_T,y $file
+done
+
+#area
+$cdo -expr,area=e1t*e2t /lus/h2resw01/hpcperm/ccpd/ECE4-DATA/nemo/domain/eORCA1/domain_cfg.nc $DIR/eORCA1/cellarea_eORCA1_T.nc
